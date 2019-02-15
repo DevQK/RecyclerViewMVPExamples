5c02339d3b689aa6eb1b4e9f155141bedc9d673a
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-02-15 16:34:48 +0700

    MainActivity

--- app/src/main/java/khanh/recyclerviewmvpexamples/MainActivity.java
+++ app/src/main/java/khanh/recyclerviewmvpexamples/MainActivity.java
@@ -1,13 +1,49 @@
 package khanh.recyclerviewmvpexamples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import khanh.recyclerviewmvpexamples.Adapter.CountryAdapter;
+import khanh.recyclerviewmvpexamples.Core.GetDataCountry;
+import khanh.recyclerviewmvpexamples.Core.Presenter;
+import khanh.recyclerviewmvpexamples.Model.CountryRss;
+
+public class MainActivity extends AppCompatActivity implements GetDataCountry.Views {
+
+    private Presenter presenter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private CountryAdapter adap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        presenter = new Presenter(this);
+        presenter.getDataFromURL(this, "");
+    }
+
+    private void initViews() {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    @Override
+    public void onGetDataSuccess(String msg, List<CountryRss> list) {
+        adap = new CountryAdapter(this, list);
+        recyclerView.setAdapter(adap);
+    }
+
+    @Override
+    public void onGetDataError(String msg) {
+        Log.d("xxxxxxx", msg);
     }
 }
